17920c88daa0ab594c262460ccabd89a3b25b5ec
Drag-On <drag-on@gmx.de>
2016-08-23 20:31:33 +0200

    Removed the now superfluous weight properties

--- include/Properties.h
+++ include/Properties.h
@@ -11,14 +11,6 @@ PROPERTIES_DEFINE(Hseg,
 	GROUP_DEFINE(clustering,
 		PROP_DEFINE(size_t, numClusters, 200u)
 	)
-	GROUP_DEFINE(weights,
-		PROP_DEFINE(float, unary, 5.f)
-		PROP_DEFINE(float, pairwise, 500.f)
-		PROP_DEFINE(float, pairwiseSigmaSq, 0.05f)
-		PROP_DEFINE(float, spGamma, 80.f)
-		PROP_DEFINE(float, spatial, 0.7f)
-		PROP_DEFINE(float, color, 1.f - spatial)
-	)
 	GROUP_DEFINE(convergence,
 		PROP_DEFINE(float, cluster, 0.001f)
 		PROP_DEFINE(float, overall, 0.001f)
