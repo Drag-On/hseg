73e2080e85c7ee4612aded6940616b056c088785
Drag-On <drag-on@gmx.de>
2016-10-14 17:08:10 +0200

    Start local training with zero vector

--- exec/train.cpp
+++ exec/train.cpp
@@ -274,7 +274,7 @@ int main()
     size_t const numClusters = properties.numClusters;
     helper::image::ColorMap const cmap = helper::image::generateColorMapVOC(std::max(256ul, numClasses));
     helper::image::ColorMap const cmap2 = helper::image::generateColorMap(properties.numClusters);
-    WeightsVec curWeights(numClasses, 5, 0, 0.08, 0.92, 0.92, 0, 230);
+    WeightsVec curWeights(numClasses, 0, 0, 0, 0, 0, 0, 0);
     if(!curWeights.read(properties.in))
         std::cout << "Couldn't read in weights to start from. Using default weights." << std::endl;
     WeightsVec oneWeights(numClasses, 1, 1, 1, 1, 1, 1, 1);
