100b25adfd8cf9b03cb42aa3a41c2da2d2454e96
Drag-On <drag-on@gmx.de>
2016-12-05 23:35:10 +0100

    Night time now starts at 22:00

--- distributed training/pickHostFile.sh
+++ distributed training/pickHostFile.sh
@@ -7,7 +7,7 @@ hour=${hour##* }
 
 if [[ $weekday == "Sun" || $weekday == "Sat" ]]; then
 	echo "weekend"
-elif [[ $hour -gt 23 || $hour -le 5 ]]; then
+elif [[ $hour -ge 22 || $hour -le 5 ]]; then
 	echo "night"
 else
 	echo "workhours"
