348179ae9e32bae861be77018918ad13a66844da
Drag-On <drag-on@gmx.de>
2016-12-08 09:07:31 +0100

    Make sure hour is interpreted as base-10 number

--- distributed training/pickHostFile.sh
+++ distributed training/pickHostFile.sh
@@ -7,7 +7,7 @@ hour=${hour##* }
 
 if [[ $weekday == "Sun" || $weekday == "Sat" ]]; then
 	echo "weekend"
-elif [[ $hour -ge 22 || $hour -le 5 ]]; then
+elif [[ $((10#$hour)) -ge 22 || $((10#$hour)) -le 5 ]]; then
 	echo "night"
 else
 	echo "workhours"
