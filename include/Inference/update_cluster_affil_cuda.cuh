3fbcc3ce1d6c85583e35b918768f7432253cd2e3
Drag-On <drag-on@gmx.de>
2017-01-23 23:23:25 +0100

    Added cuda implementation of cluster affiliation update step

--- include/Inference/update_cluster_affil_cuda.cuh
+++ include/Inference/update_cluster_affil_cuda.cuh
@@ -2,4 +2,28 @@
 #ifndef UPDATE_CLUSTER_AFFIL_CUDA
 #define UPDATE_CLUSTER_AFFIL_CUDA
 
+#include <Image/FeatureImage.h>
+#include <thrust/device_vector.h>
+#include <Image/Image.h>
+#include "Cluster.h"
+
+template<typename EnergyFun>
+class UpdateClusterAffilCuda
+{
+public:
+    UpdateClusterAffilCuda(EnergyFun const& energyFun, FeatureImage const* features);
+
+    void updateClusterAffiliation(LabelImage& outClustering, LabelImage const& labeling, std::vector<Cluster> const& clusters);
+
+private:
+    thrust::device_vector<float> m_features_device;
+    thrust::device_vector<float> m_ho_weights_device;
+    thrust::device_vector<float> m_feat_weights_device;
+    Label m_numLabels;
+    uint32_t m_feat_dim;
+};
+
+
+void test();
+
 #endif //UPDATE_CLUSTER_AFFIL_CUDA
